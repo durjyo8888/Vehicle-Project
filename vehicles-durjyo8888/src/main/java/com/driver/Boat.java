b005dcdf0557fcff51857d75f9a512c6d2670d96
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-07-05 20:01:07 +0530

    Add files via upload
    git init

---
+++ vehicles-durjyo8888/src/main/java/com/driver/Boat.java
@@ -0,0 +1,41 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(){
+        super();
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
